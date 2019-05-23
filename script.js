196759f832313268d569c2f5a19630a95009c1e3
Yuriy Mironov <git>
2019-05-23 14:29:25 +0400

    [v] Завершено

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     const el_btn = document.querySelector('#start_game');
+
     if (el_btn) {
         el_btn.addEventListener('click', function () {
             startGame();
@@ -33,8 +34,6 @@ function createTable() {
                 for (j = 0; j < 4; j++) {
                     let cell = row.insertCell(j);
 
-
-                    cell.innerHTML = colors$[k];
                     cell.classList.add('colors' + colors$[k]);
                     cell.classList.add(k);
                     cell.classList.add('invisible');
@@ -57,49 +56,90 @@ let score = 0;
 
 function getCell(el) {
 
+    if (gameIsStarted) {
 
-    if (!el.classList.contains('done')) {
 
-        el.classList.remove('invisible');
+        if (!el.classList.contains('done')) {
 
-        if (first === '') { first = el } else { second = el };
+            el.classList.remove('invisible');
 
-        let x = first;
-        let y = second;
+            if (first === '') { first = el } else { second = el };
 
-        if (first !== '' && second !== '') {
-            if (first.classList[0] === second.classList[0]) {
+            let x = first;
+            let y = second;
 
-                score++;
+            if (first !== '' && second !== '') {
+                if (first.classList[0] === second.classList[0]) {
 
-                first.classList.add('done');
-                second.classList.add('done');
+                    score++;
 
-            } else {
+                    first.classList.add('done');
+                    second.classList.add('done');
 
-                setTimeout(function () {
-                    x.classList.add('invisible');
-                    y.classList.add('invisible');
-                }, 500);
+                } else {
+
+                    setTimeout(function () {
+                        x.classList.add('invisible');
+                        y.classList.add('invisible');
+                    }, 500);
+                }
+
+                first = '';
+                second = '';
             }
-
-            first = '';
-            second = '';
         }
     }
 
-    if (score === 8) alert('вы выиграли')
+    if (score === 8) {
+        clearInterval(gameTimer);
+
+        const el_strt = document.querySelector('.started');
+        el_strt.classList.remove('started_on');
+        el_strt.classList.add('started_off');
+
+        gameIsStarted = false;
+        alert('Вы выиграли\nЗатраченное время: ' + tmr);
+    }
+
 }
 
-
-
-function startTimer(action) {
-
-    
-}
+let sec = 0;
+let min = 0;
+let hour = 0;
+let tmr = '';
+let gameIsStarted = false;
+let gameTimer;
 
 function startGame() {
+    gameIsStarted = true;
 
-    console.log('game ready to go!');
-    // startTimer(start);
+    const el_strt = document.querySelector('.started');
+    const el_sec = document.querySelector('#sec');
+    const el_min = document.querySelector('#min');
+    const el_hour = document.querySelector('#hour');
+
+    if (el_strt) {
+
+        if (el_strt.classList.contains('started_on')) {
+            el_strt.classList.remove('started_on');
+            el_strt.classList.add('started_off');
+        } else {
+
+
+            el_strt.classList.remove('started_off');
+            el_strt.classList.add('started_on');
+
+            gameTimer = setInterval(function () {
+                sec++;
+                if (sec === 60) { min++; sec = 0 };
+                if (min === 60) { hour++; min = 0 };
+                if (hour < 10) { el_hour.innerHTML = '0' + hour } else { el_hour.innerHTML = hour };
+                if (min < 10) { el_min.innerHTML = '0' + min } else { el_min.innerHTML = min };
+                if (sec < 10) { el_sec.innerHTML = '0' + sec } else { el_sec.innerHTML = sec };
+                tmr = el_hour.innerHTML + ' : ' + el_min.innerHTML + ' : ' + el_sec.innerHTML;
+            }, 1000);
+
+        }
+
+    }
 }
